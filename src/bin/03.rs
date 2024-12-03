85450c2e694fe22e59b68d0b5be37970f367ac9a
Trent Gardner <trent.gardner@gmail.com>
2024-12-03 19:58:26 +1030

    Added Day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,24 +1,43 @@
 advent_of_code::solution!(3);
+
 use regex::Regex;
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+fn run_program(input: &str, cmd_pattern: &Regex) -> i32 {
+    let mut running = true;
+    let mut total = 0;
 
-    let mut mul = Vec::<(i32, i32)>::new();
-
-    for captures in re.captures_iter(input) {
-        let first_num = captures.get(1).unwrap().as_str().parse().unwrap();
-        let second_num = captures.get(2).unwrap().as_str().parse().unwrap();
-        mul.push((first_num, second_num));
+    for captures in cmd_pattern.captures_iter(input) {
+        if let Some(cmd) = captures.get(0) {
+            let cstr = cmd.as_str().split('(').next().unwrap();
+            match cstr {
+                "do" => running = true,
+                "don't" => running = false,
+                "mul" => {
+                    if running {
+                        total += captures.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                            * captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 
-    let result = mul.iter().map(|f| f.0 * f.1).sum::<i32>();
-
-    return Some(result as u32);
+    total
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+    let pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let result = run_program(input, &pattern);
+
+    return Some(result);
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    let pattern = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let result = run_program(input, &pattern);
+
+    Some(result)
 }
 
 #[cfg(test)]
