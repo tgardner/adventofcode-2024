d59bf06f05922dd6869fd550ee3b8123cfdf74c9
Trent Gardner <trent.gardner@gmail.com>
2024-12-10 20:35:52 +1030

    Speed improvements

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,72 +1,114 @@
+use std::collections::BTreeMap;
+
 advent_of_code::solution!(9);
 
-fn solve(mut files: Vec<(usize, i32)>) -> usize {
-    let mut i = files.len() - 1;
-    while i > 0 {
-        let (size, id) = files[i];
-        if id == -1 {
-            i -= 1;
-            continue;
-        }
-        if let Some(j) = files[0..i]
-            .iter()
-            .position(|&(s, id)| id == -1 && size <= s)
-        {
-            let s = files[j].0;
-            files[j] = (size, id);
-            files[i] = (size, -1);
-            if size < s {
-                files.insert(j + 1, (s - size, -1));
+fn parse_blocks(raw_input: &str) -> Vec<Option<usize>> {
+    let mut blocks = Vec::new();
+
+    let mut file_id: usize = 0;
+    for (idx, c) in raw_input.chars().enumerate() {
+        if let Some(digit) = c.to_digit(10) {
+            let block = if idx % 2 == 0 {
+                let b = Some(file_id);
+                file_id += 1;
+                b
+            } else {
+                None
+            };
+
+            for _ in 0..digit {
+                blocks.push(block);
             }
+        } else {
+            break;
         }
-        i -= 1;
     }
-    files
-        .iter()
-        .flat_map(|&(s, id)| (0..s).map(move |_| id))
-        .enumerate()
-        .map(|(i, id)| if id == -1 { 0 } else { i * id as usize })
-        .sum()
+
+    blocks
+}
+
+fn parse_files_and_free_spaces(raw_input: &str) -> (Vec<(usize, usize)>, Vec<(usize, usize)>) {
+    let mut file_layout = Vec::new();
+    let mut free_space_layout = Vec::new();
+
+    let mut block_id: usize = 0;
+    for (idx, c) in raw_input.chars().enumerate() {
+        if let Some(size) = c.to_digit(10) {
+            if size > 0 {
+                let size = size as usize;
+                if idx % 2 == 0 {
+                    file_layout.push((block_id, size));
+                } else {
+                    free_space_layout.push((block_id, size));
+                }
+
+                block_id += size;
+            }
+        } else {
+            break;
+        }
+    }
+
+    (file_layout, free_space_layout)
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut fs1 = Vec::new();
-    let mut fid = 0;
+    let mut blocks = parse_blocks(input);
 
-    for (i, b) in input.bytes().enumerate() {
-        if b.is_ascii_alphanumeric() {
-            let v = if i % 2 == 0 {
-                fid += 1;
-                fid - 1
-            } else {
-                -1
-            };
-            fs1.extend((0..b - b'0').map(|_| (1, v)));
+    // compaction with fragmentation
+    let mut block_id = 0;
+    let mut checksum = 0;
+
+    while block_id < blocks.len() {
+        if let Some(file_id) = blocks[block_id] {
+            checksum += file_id * block_id;
+            block_id += 1;
+        } else if let Some(file_id) = blocks.pop().unwrap() {
+            blocks[block_id] = Some(file_id);
         }
     }
-
-    let p1 = solve(fs1);
-    Some(p1)
+    Some(checksum as usize)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut fs2 = Vec::new();
-    let mut fid = 0;
+    // files =>  block_id, size
+    // free spaces => block_id, size
+    let (files, free_spaces) = parse_files_and_free_spaces(input);
 
-    for (i, b) in input.bytes().enumerate() {
-        if b.is_ascii_alphanumeric() {
-            let v = if i % 2 == 0 {
-                fid += 1;
-                fid - 1
-            } else {
-                -1
-            };
-            fs2.push(((b - b'0') as usize, v));
+    // maps free space starting at block X to free space size
+    let mut free_spaces_by_block_id: BTreeMap<usize, usize> = BTreeMap::from_iter(free_spaces);
+
+    let mut checksum = 0;
+
+    // compaction without fragmentation
+    for (file_id, &(file_block_id, file_size)) in files.iter().enumerate().rev() {
+        // are there any free blocks before this file that can fit it?
+        let free_space = free_spaces_by_block_id
+            .iter()
+            .take_while(|&(&block_id, _)| block_id < file_block_id)
+            .find(|&(_, &size)| size >= file_size);
+
+        let mut new_file_block_id = file_block_id;
+
+        // If we found a free space...
+        if let Some((&free_space_block_id, &free_space_size)) = free_space {
+            // 1. store the file in the free space
+            new_file_block_id = free_space_block_id;
+
+            // 2. remove the free space we've used
+            free_spaces_by_block_id.remove(&free_space_block_id);
+
+            // 3. add any remaining space behind the file back into the free spaces btree
+            if free_space_size > file_size {
+                free_spaces_by_block_id
+                    .insert(free_space_block_id + file_size, free_space_size - file_size);
+            }
         }
-    }
 
-    let p2 = solve(fs2);
-    Some(p2)
+        // use arithmetic series formula for checksum
+        checksum += (file_id as usize) * (new_file_block_id * 2 + file_size - 1) * file_size / 2;
+    }
+    Some(checksum)
 }
 
 #[cfg(test)]
