78515eac27fd6209b2b6065a78853563478df2cf
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2024-12-16 15:10:01 +1030

    Slight refactor

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -22,19 +22,40 @@ impl From<&str> for Warehouse {
             .next()
             .unwrap()
             .chars()
-            .filter(|c| "^>v<".contains(*c))
             .filter_map(|c| Direction::try_from(c).ok())
             .collect();
+        let robot = find_robot(&map);
 
-        Self {
-            map,
-            moves,
-            robot: (0, 0),
-        }
+        Self { map, moves, robot }
     }
 }
 
 impl Warehouse {
+    fn from_wide(input: &str) -> Self {
+        let mut warehouse = Warehouse::from(input);
+
+        warehouse.map = warehouse
+            .map
+            .clone()
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|cell| match cell {
+                        '#' => vec!['#', '#'],
+                        '.' => vec!['.', '.'],
+                        'O' => vec!['[', ']'],
+                        '@' => vec!['@', '.'],
+                        _ => vec![],
+                    })
+                    .flatten()
+                    .collect()
+            })
+            .collect();
+        warehouse.robot = find_robot(&warehouse.map);
+
+        warehouse
+    }
+
     #[allow(dead_code)]
     fn print(&self) {
         for row in self.map.iter() {
@@ -45,20 +66,7 @@ impl Warehouse {
         }
     }
 
-    fn find_robot(&mut self) {
-        for (row, line) in self.map.iter().enumerate() {
-            for (col, &cell) in line.iter().enumerate() {
-                if cell == '@' {
-                    self.robot = (col, row);
-                    break;
-                }
-            }
-        }
-    }
-
     fn patrol(&mut self) {
-        self.find_robot();
-
         for d in self.moves.clone().iter() {
             let (x, y) = self.robot;
 
@@ -119,37 +127,35 @@ impl Warehouse {
     }
 
     fn gps(&self) -> usize {
-        let mut coord = 0;
-        for row in 0..self.map.len() {
-            for col in 0..self.map[row].len() {
-                let cell = self.map[row][col];
-                if cell == 'O' || cell == '[' {
-                    coord += 100 * row + col;
-                }
+        self.map
+            .iter()
+            .enumerate()
+            .flat_map(|(r, row)| {
+                row.iter().enumerate().map(
+                    move |(c, cell)| {
+                        if "O[".contains(*cell) {
+                            100 * r + c
+                        } else {
+                            0
+                        }
+                    },
+                )
+            })
+            .sum()
+    }
+}
+
+fn find_robot(grid: &Vec<Vec<char>>) -> (usize, usize) {
+    let mut robot = (0, 0);
+    for (row, line) in grid.iter().enumerate() {
+        for (col, &cell) in line.iter().enumerate() {
+            if cell == '@' {
+                robot = (col, row);
+                break;
             }
         }
-        coord
     }
-
-    fn stretch(&mut self) {
-        self.map = self
-            .map
-            .clone()
-            .iter()
-            .map(|row| {
-                row.iter()
-                    .map(|cell| match cell {
-                        '#' => vec!['#', '#'],
-                        '.' => vec!['.', '.'],
-                        'O' => vec!['[', ']'],
-                        '@' => vec!['@', '.'],
-                        _ => vec![],
-                    })
-                    .flatten()
-                    .collect()
-            })
-            .collect()
-    }
+    robot
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -159,8 +165,7 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut warehouse = Warehouse::from(input);
-    warehouse.stretch();
+    let mut warehouse = Warehouse::from_wide(input);
     warehouse.patrol();
     Some(warehouse.gps())
 }
