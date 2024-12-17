83c363c2b58a7b9ef786dd9141050b55f6b8ecd8
Trent Gardner <trent.gardner@healthcareaustralia.com.au>
2024-12-17 20:33:40 +1030

    Rewrite day 17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,163 +1,101 @@
-use std::collections::VecDeque;
-
 advent_of_code::solution!(17);
 
-struct Computer {
-    registers: [isize; 3],
-    program: Vec<u8>,
-    pointer: usize,
-    output: VecDeque<u8>,
+struct Computer<'a> {
+    program: &'a [u64],
+    ip: usize,
+    a: u64,
+    b: u64,
+    c: u64,
 }
 
-impl Computer {
-    fn new(a: isize, b: isize, c: isize, program: Vec<u8>) -> Self {
-        Self {
-            registers: [a, b, c],
-            program,
-            pointer: 0,
-            output: VecDeque::new(),
-        }
-    }
+impl Computer<'_> {
+    fn run(&mut self) -> Option<u64> {
+        while self.ip < self.program.len() {
+            let combo = |index| match self.program[index] {
+                0..4 => self.program[index],
+                4 => self.a,
+                5 => self.b,
+                6 => self.c,
+                _ => unreachable!(),
+            };
 
-    fn run(&mut self) -> String {
-        while self.pointer < self.program.len() {
-            let opcode = self.program[self.pointer];
-            let operand = self.program[self.pointer + 1];
-            match opcode {
-                0 => self.adv(operand),
-                1 => self.bxl(operand),
-                2 => self.bst(operand),
-                3 => self.jnz(operand),
-                4 => self.bxc(),
-                5 => self.out(operand),
-                6 => self.bdv(operand),
-                7 => self.cdv(operand),
-                _ => panic!("Unknown opcode: {}", opcode),
+            match self.program[self.ip] {
+                0 => self.a >>= combo(self.ip + 1),
+                1 => self.b ^= self.program[self.ip + 1],
+                2 => self.b = combo(self.ip + 1) % 8,
+                3 => {
+                    if self.a != 0 {
+                        self.ip = self.program[self.ip + 1] as usize;
+                        continue;
+                    }
+                }
+                4 => self.b ^= self.c,
+                5 => {
+                    let out = combo(self.ip + 1) % 8;
+                    self.ip += 2;
+                    return Some(out);
+                }
+                6 => self.b = self.a >> combo(self.ip + 1),
+                7 => self.c = self.a >> combo(self.ip + 1),
+                _ => unreachable!(),
             }
-            if opcode != 3 || self.registers[0] == 0 {
-                self.pointer += 2;
-            }
+
+            self.ip += 2;
         }
-        self.output.iter().map(|&v| v.to_string()).collect::<Vec<_>>().join(",")
-    }
 
-    fn get_combo_operand(&self, operand: u8) -> isize {
-        match operand {
-            0..=3 => operand as isize,
-            4 => self.registers[0],
-            5 => self.registers[1],
-            6 => self.registers[2],
-            7 => panic!("Invalid combo operand 7"),
-            _ => unreachable!(),
-        }
-    }
-
-    fn adv(&mut self, operand: u8) {
-        let divisor = 2_isize.pow(self.get_combo_operand(operand) as u32);
-        self.registers[0] /= divisor;
-    }
-
-    fn bxl(&mut self, operand: u8) {
-        self.registers[1] ^= operand as isize;
-    }
-
-    fn bst(&mut self, operand: u8) {
-        let value = self.get_combo_operand(operand) % 8;
-        self.registers[1] = value;
-    }
-
-    fn jnz(&mut self, operand: u8) {
-        if self.registers[0] != 0 {
-            self.pointer = operand as usize;
-        }
-    }
-
-    fn bxc(&mut self) {
-        self.registers[1] ^= self.registers[2];
-    }
-
-    fn out(&mut self, operand: u8) {
-        let value = self.get_combo_operand(operand) % 8;
-        self.output.push_back(value as u8);
-    }
-
-    fn bdv(&mut self, operand: u8) {
-        let divisor = 2_isize.pow(self.get_combo_operand(operand) as u32);
-        self.registers[1] = self.registers[0] / divisor;
-    }
-
-    fn cdv(&mut self, operand: u8) {
-        let divisor = 2_isize.pow(self.get_combo_operand(operand) as u32);
-        self.registers[2] = self.registers[0] / divisor;
+        None
     }
 }
 
-impl From<&str> for Computer {
-    fn from(value: &str) -> Self {
-        let mut registers = [0; 3];
-        let mut program = Vec::new();
-    
-        for line in value.lines() {
-            if line.starts_with("Register A") {
-                registers[0] = line.split(": ").nth(1).unwrap().parse::<isize>().unwrap();
-            } else if line.starts_with("Register B") {
-                registers[1] = line.split(": ").nth(1).unwrap().parse::<isize>().unwrap();
-            } else if line.starts_with("Register C") {
-                registers[2] = line.split(": ").nth(1).unwrap().parse::<isize>().unwrap();
-            } else if line.starts_with("Program") {
-                program = line
-                    .split(": ")
-                    .nth(1)
-                    .unwrap()
-                    .split(',')
-                    .map(|num| num.parse::<u8>().unwrap())
-                    .collect();
-            }
-        }
-    
-        Computer {
-            registers,
-            program,
-            pointer: 0,
-            output: VecDeque::new(),
-        }
-    }
-}
-
-
-fn find_lowest_a(program: &[u8]) -> isize {
-    let mut a = 0;
-    for n in 1..=program.len() {
-        let target = program[program.len()-n..]
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(",");
-        let mut new_a = a << 3;
-        loop {
-            let mut comp = Computer::new(new_a, 0, 0, program.to_vec());
-            let output = comp.run();
-            
-            if output == target {
-                a = new_a;
-                break;
-            }
-            new_a += 1;
-        }
-    }
-    a
+pub fn parse(input: &str) -> Vec<u64> {
+    input.split(|c: char| !c.is_digit(10)) // Split on non-digits
+        .filter(|s| !s.is_empty()) // Remove empty strings
+        .filter_map(|s| s.parse::<u64>().ok()) // Parse and filter out errors
+        .collect()
 }
 
 pub fn part_one(input: &str) -> Option<String> {
-    let mut computer = Computer::from(input);
-    let result = computer.run();
+    let input = parse(input);
+    let mut computer =
+        Computer { program: &input[3..], ip: 0, a: input[0], b: input[1], c: input[2] };
+    let mut out = Vec::new();
+
+    while let Some(n) = computer.run() {
+        let digit = (n as u8 + b'0') as char;
+        out.push(digit);
+        out.push(',');
+    }
+
+    out.pop();
+    let result = out.iter().collect();
     Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<isize> {
-    let computer = Computer::from(input);
-    let result = find_lowest_a(&computer.program);
-    Some(result)
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut valid = vec![0];
+    let input = parse(input);
+
+    for &out in input.iter().skip(3).rev() {
+        let mut next = Vec::new();
+
+        for v in valid {
+            for n in 0..8 {
+                let a = (v << 3) | n;
+                let mut computer =
+                    Computer { program: &input[3..], ip: 0, a, b: input[1], c: input[2] };
+
+                if let Some(result) = computer.run() {
+                    if result == out {
+                        next.push(a);
+                    }
+                }
+            }
+        }
+
+        valid = next;
+    }
+
+    valid.iter().min().copied()
 }
 
 #[cfg(test)]
